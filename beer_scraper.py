64ffa2f6e397614f5eb6cfdc653b4bcab59e61d2
dijkstrar <52031414+dijkstrar@users.noreply.github.com>
2020-08-25 01:39:29 +0200

    Update beer_scraper.py

--- beer_scraper.py
+++ beer_scraper.py
@@ -27,8 +27,7 @@ def albert_heijn(driver) -> dict:
     beer_urls = {'Heineken':'https://www.ah.nl/producten/product/wi210145/heineken-premium-pilsener',
            'Grolsch':'https://www.ah.nl/producten/product/wi232949/grolsch-premium-pilsner-krat',
            'Hertog Jan':'https://www.ah.nl/producten/product/wi2708/hertog-jan-traditioneel-natuurzuiver-bier',
-           'Brand':'https://www.ah.nl/producten/product/wi227163/brand-pilsener',
-           'Warsteiner':'https://www.ah.nl/producten/product/wi126867/warsteiner-pilsener'}
+           'Brand':'https://www.ah.nl/producten/product/wi227163/brand-pilsener'}
     
     def extract_price(soup: BeautifulSoup):
         full =float(soup.select_one("span[class*='price-amount_integer']").text)
@@ -110,7 +109,6 @@ def jumbo(driver):
            'Grolsch':'https://www.jumbo.com/grolsch-premium-pilsner-fles-24-x-30cl/147464KRT',
            'Hertog Jan':'https://www.jumbo.com/hertog-jan-traditioneel-natuurzuiver-bier-krat-24-x-30cl/865788KRT',
            'Brand':'https://www.jumbo.com/brand-bier-krat-24-x-30cl/140388KRT',
-           'Warsteiner':'https://www.jumbo.com/warsteiner-krat-24-x-300ml/449245KRT'
                 }
     def extract_price(soup: BeautifulSoup):
         full = float(soup.select_one('span[class*="jum-product-price__current-price--larger"]').text)
@@ -134,10 +132,9 @@ def jumbo(driver):
         result_dict[beer] = info
     return result_dict
 
-def fetch_minima():
-    minima = {}
+def fetch_prices():
     brands = ['Heineken','Grolsch','Brand','Hertog Jan']
-    with get_firefox() as driver:
+    with get_chrome() as driver:
         ah_prices = albert_heijn(driver)
         dirk_prices = dirk(driver)
         jumbo_prices = jumbo(driver)
@@ -148,20 +145,9 @@ def fetch_minima():
         date = pd.to_datetime("today").strftime("%Y/%m/%d")
         file.write(date+';'+str(ah_prices[brand])+';'+str(dirk_prices[brand])+';'+str(jumbo_prices[brand])+'\n')
         file.close()
-    
-    #minimum detection
-    for brand in brands:
-        minimum_price = min(ah_prices[brand],dirk_prices[brand],jumbo_prices[brand])
-        if (brand,minimum_price) in ah_prices.items():
-            supermarket = 'Albert Heijn'
-        elif (brand,minimum_price) in dirk_prices.items():
-            supermarket = 'Dirk'
-        else: supermarket = 'Jumbo'
-        minima[brand] = (min(ah_prices[brand],dirk_prices[brand],jumbo_prices[brand]),supermarket)
-    return minima
 
 if __name__ == '__main__':
     start_time=time.time()
     print('---FETCHING PRICES---')
-    print(fetch_minima())
+    fetch_prices()
     print("--- %s seconds ---"% (time.time()-start_time))
