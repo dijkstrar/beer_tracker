adca765fede721f2ca71921c3ea33cc102952e53
dijkstrar <52031414+dijkstrar@users.noreply.github.com>
2020-08-25 00:41:46 +0200

    Determining of minima prices and file writings

--- beer_scraper.py
+++ beer_scraper.py
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup
 from contextlib import contextmanager
 import time
 import re
+import pandas as pd
 
 
 @contextmanager
@@ -25,7 +26,6 @@ def albert_heijn(driver) -> dict:
     
     beer_urls = {'Heineken':'https://www.ah.nl/producten/product/wi210145/heineken-premium-pilsener',
            'Grolsch':'https://www.ah.nl/producten/product/wi232949/grolsch-premium-pilsner-krat',
-           'Grolsch Beugel':'https://www.ah.nl/producten/product/wi2724/grolsch-premium-pilsner-beugelfles',
            'Hertog Jan':'https://www.ah.nl/producten/product/wi2708/hertog-jan-traditioneel-natuurzuiver-bier',
            'Brand':'https://www.ah.nl/producten/product/wi227163/brand-pilsener',
            'Warsteiner':'https://www.ah.nl/producten/product/wi126867/warsteiner-pilsener'}
@@ -58,7 +58,6 @@ def albert_heijn(driver) -> dict:
 
     result_dict = {}
     for (beer,url) in beer_urls.items():
-        print(url)
         results_html=visit_page(url,driver)
         soup = BeautifulSoup(results_html, 'html.parser')
         info = extract_price(soup)
@@ -69,10 +68,8 @@ def albert_heijn(driver) -> dict:
 def dirk(driver):
     beer_urls = {'Heineken':'https://www.dirk.nl/boodschappen/dranken-sap-koffie-thee/bier/heineken-pilsener/6',
            'Grolsch':'https://www.dirk.nl/boodschappen/dranken-sap-koffie-thee/bier/grolsch-premium-pilsener-krat/8993',
-#            'Grolsch Beugel':'https://www.ah.nl/producten/product/wi2724/grolsch-premium-pilsner-beugelfles',
            'Hertog Jan':'https://www.dirk.nl/boodschappen/dranken-sap-koffie-thee/bier/hertog-jan-pilsener/9486',
            'Brand':'https://www.dirk.nl/boodschappen/dranken-sap-koffie-thee/bier/brand-pilsener/8359',
-#            'Warsteiner':'https://www.ah.nl/producten/product/wi126867/warsteiner-pilsener'
                 }
     def extract_price(soup: BeautifulSoup, discount):
         if discount:
@@ -101,7 +98,6 @@ def dirk(driver):
 
     result_dict = {}
     for (beer,url) in beer_urls.items():
-        print(url)
         results_html,discount=visit_page(url,driver)
         soup = BeautifulSoup(results_html, 'html.parser')
         info = extract_price(soup,discount)
@@ -112,7 +108,6 @@ def dirk(driver):
 def jumbo(driver):
     beer_urls = {'Heineken':'https://www.jumbo.com/heineken-premium-pilsener-krat-24-x-30cl/87441KRT',
            'Grolsch':'https://www.jumbo.com/grolsch-premium-pilsner-fles-24-x-30cl/147464KRT',
-           'Grolsch Beugel':'https://www.jumbo.com/grolsch-premium-pilsner-krat-16-x-45cl/508102KRT',
            'Hertog Jan':'https://www.jumbo.com/hertog-jan-traditioneel-natuurzuiver-bier-krat-24-x-30cl/865788KRT',
            'Brand':'https://www.jumbo.com/brand-bier-krat-24-x-30cl/140388KRT',
            'Warsteiner':'https://www.jumbo.com/warsteiner-krat-24-x-300ml/449245KRT'
@@ -125,6 +120,7 @@ def jumbo(driver):
     def visit_page(url,driver):
         driver.get(url)
         results_selector = "span[class*='jum-product-price__current-price']"
+        time.sleep(1)
         results_el = driver.find_element_by_css_selector(results_selector)
         results_html = results_el.get_attribute('outerHTML')
         return results_html
@@ -132,23 +128,40 @@ def jumbo(driver):
     
     result_dict = {}
     for (beer,url) in beer_urls.items():
-        print(url)
         results_html=visit_page(url,driver)
         soup = BeautifulSoup(results_html, 'html.parser')
         info = extract_price(soup)
         result_dict[beer] = info
     return result_dict
 
-def fetch_all():
-    with get_chrome() as driver:
-        print(albert_heijn(driver))
-        print(dirk(driver))
-        print(jumbo(driver))
+def fetch_minima():
+    minima = {}
+    brands = ['Heineken','Grolsch','Brand','Hertog Jan']
+    with get_firefox() as driver:
+        ah_prices = albert_heijn(driver)
+        dirk_prices = dirk(driver)
+        jumbo_prices = jumbo(driver)
+    
+    #write files
+    for brand in brands:
+        file = open('logs/'+brand+'.txt','a')
+        date = pd.to_datetime("today").strftime("%Y/%m/%d")
+        file.write(date+';'+str(ah_prices[brand])+';'+str(dirk_prices[brand])+';'+str(jumbo_prices[brand])+'\n')
+        file.close()
+    
+    #minimum detection
+    for brand in brands:
+        minimum_price = min(ah_prices[brand],dirk_prices[brand],jumbo_prices[brand])
+        if (brand,minimum_price) in ah_prices.items():
+            supermarket = 'Albert Heijn'
+        elif (brand,minimum_price) in dirk_prices.items():
+            supermarket = 'Dirk'
+        else: supermarket = 'Jumbo'
+        minima[brand] = (min(ah_prices[brand],dirk_prices[brand],jumbo_prices[brand]),supermarket)
+    return minima
 
 if __name__ == '__main__':
     start_time=time.time()
     print('---FETCHING PRICES---')
-    fetch_all()
+    print(fetch_minima())
     print("--- %s seconds ---"% (time.time()-start_time))
-    
-    
